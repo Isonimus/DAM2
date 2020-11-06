bb750bebc29e9f510c39239d16404f981a824796
Isonimus <multi@AFEQUIPO10>
2020-11-06 16:29:38 +0100

    Ejemplo de productor-consumidor con un buffer (tubería) en el monitor.

---
+++ Tuberia/src/tuberia/Launcher.java
@@ -0,0 +1,14 @@
+package tuberia;
+
+class Launcher {
+	
+    public static void main( String args[] ) {
+    	
+        Tuberia t = new Tuberia();
+        Productor p = new Productor( t );
+        Consumidor c = new Consumidor( t );
+
+        p.start();
+        c.start();
+    }
+}
