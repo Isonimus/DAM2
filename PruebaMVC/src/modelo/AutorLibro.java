72b32195143b5b1b6d1124d1f9ffe2062c3591f6
Isonimus <multi@AFEQUIPO10>
2020-11-20 20:48:45 +0100

    UPDATE 20112020

--- PruebaMVC/src/modelo/AutorLibro.java
+++ PruebaMVC/src/modelo/AutorLibro.java
@@ -106,7 +106,6 @@ public class AutorLibro extends DAO{
 		return buscaResultadosConConsulta("Select cod_autor, isbn from autor_libro where isbn = " + isbn);
 	}
 	
-	
 	//LEER POR AUTOR
 	public static Vector<AutorLibro> buscarPorCodAutor(int codAutor) throws SQLException{
 				
