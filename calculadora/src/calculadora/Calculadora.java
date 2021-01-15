34f7d319a56c6c7ab55affceb586904d26806140
Isonimus <multi@AFEQUIPO10>
2021-01-15 19:05:42 +0100

    Ejemplo de web service sencillo.

---
+++ calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+package calculadora;
+
+public class Calculadora {
+	
+	public static int sumar(int a, int b) {
+		return a+b;
+	}
+	
+	public static int restar(int a, int b) {
+		return a-b;
+	}
+	
+	public static int multiplicar(int a, int b) {
+		return a*b;
+	}
+	
+	public static float dividir(int a, int b) {
+		return a/b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
